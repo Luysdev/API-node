e6d998a8f9fd996130b9f898f4760132fd0c5aac
Luís Eduardo da Silva Batista <luiseduardosbatista2@gmail.com>
2023-08-19 19:16:28 -0300

    Finalização das rotas

--- API-REST/src/app.js
+++ API-REST/src/app.js
@@ -1,13 +1,10 @@
 import express from 'express';
-import SelecaoController from './app/controllers/SelecaoController.js';
-const app = express();
+import router from './routes.js';
 
+
+const app = express();
+app.use(router)
 app.use(express.json());
 
-app.get('/selecoes', SelecaoController.index)
-app.get('/selecoes/:id', SelecaoController.show )
-app.post('/selecoes', SelecaoController.store)
-app.put('/selecoes/:id', SelecaoController.uptdate)
-app.delete('/selecoes/:id', SelecaoController.delete)
 
 export default app
