3f89e227ae33c57089eb9a660e3f0b1564a0a3a7
Luís Eduardo da Silva Batista <luiseduardosbatista2@gmail.com>
2023-08-19 04:12:58 -0300

    banco de dados finalizado

--- API-REST/server.js
+++ API-REST/server.js
@@ -9,10 +9,10 @@ dbConex.connect((erro) => {
         console.log(erro)
     }else{
         console.log("Conexão realizada com sucesso")
+        //subir o servidor
         app.listen(PORT,()=>{
             console.log(`servidor rodando no endereço http://localhost:${PORT}`);
         })
     }
 })
 
-//subir o servidor
--- API-REST/src/app.js
+++ API-REST/src/app.js
@@ -1,26 +1,70 @@
 import express from 'express';
+import dbConex from '../db/db.js';
 
 const app = express();
 
 //Faz com que o express leia o body com o json
 app.use(express.json());
 
-//mock
-
-
-//requisita uma rota raiz http, toda requisição precisa de um res e um request
-app.get('/', (req,res)=>{
-    res.send('CONECTADO');
-})
-
 app.get('/selecoes', (req,res)=>{
-    res.status(200).send(selecoes)
+    const sql = "SELECT * FROM selecoes"
+    dbConex.query(sql, (erro,resultado) => {
+        if(erro) {
+            res.status(404).json({'erro' : erro})
+        }else {
+            res.status(200).json(resultado);
+        }
+    })
 })
 
-// Uso o metodo post para receber uma informação através do request 
-app.post('/selecoes', (req,res)=>{
-    selecoes.push(req.body);
-    res.status(201).send('Seleção cadastrada com sucesso');
+app.get('/selecoes/:id', (req,res) => {
+    const id = req.params.id;
+    const sql = "SELECT * FROM selecoes WHERE id=?"
+    dbConex.query(sql,id,(erro,resultado) => {
+        if(erro) {
+            res.status(404).json({'erro' : erro})
+        }else {
+            res.status(200).json(resultado);
+        }
+    })
 })
+
+app.post('/selecoes', (req,res) => {
+    const selecao  = req.body;
+    const sql = "INSERT INTO selecoes SET ?;"
+    const values = { selecoes: selecao.selecao, grupo: selecao.grupo }; 
+    dbConex.query(sql,values,(erro,resultado) => {
+        if(erro) {
+            res.status(400).json({'erro' : erro})
+        }else {
+            res.status(201).json(resultado);
+        }
+    })
+
+})
+
+app.delete('/selecoes/:id', (req,res)=>{
+    const id = req.params.id
+    const sql = "DELETE FROM selecoes WHERE id=?"
+    dbConex.query(sql, id,(erro,resultado) =>{
+        if(erro){
+            res.status(404).json({'erro':erro})
+        }else{
+            res.status(200).json(resultado);
+        }
+    })
+})
+
+app.put('/selecoes/:id', (req,res) => {
+    const id = req.params.id
+    const selecao = req.body
+    const sql = "UPDATE selecoes SET ? WHERE id=?"
+    dbConex.query(sql,[selecao,id],(erro,resultado) =>{
+        if(erro){
+            res.status(404).json({'erro':erro})
+        }else{
+            res.status(200).json(resultado);
+        }
+})})
 
 export default app
